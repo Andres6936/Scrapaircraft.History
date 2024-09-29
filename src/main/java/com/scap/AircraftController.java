2a99df24d9b33df1e8e1109615ae1de4e7808835
Andres6936 <andres6936@gmail.com>
2024-09-28 20:21:38 -0500

    Add HTTP method annotations for CRUD operations
    Implemented @Get, @Put, @Post, and @Delete annotations to define corresponding endpoints for finding, saving, updating, and deleting aircraft data. This enhances the readability and functionality of the AircraftController class.

--- src/main/java/com/scap/AircraftController.java
+++ src/main/java/com/scap/AircraftController.java
@@ -2,29 +2,32 @@ package com.scap;
 
 import io.micronaut.http.HttpResponse;
 import io.micronaut.http.HttpStatus;
-import io.micronaut.http.annotation.Body;
-import io.micronaut.http.annotation.Controller;
-
-import java.util.ArrayList;
+import io.micronaut.http.annotation.*;
 
 @Controller("/aircraft")
 public class AircraftController {
+
+    @Get("/findOne")
     public HttpResponse<Aircraft> findOne() {
         return HttpResponse.status(HttpStatus.OK).body(new Aircraft());
     }
 
+    @Get("/findMany")
     public HttpResponse<Aircraft[]> findAll() {
         return HttpResponse.status(HttpStatus.OK).body(new Aircraft[]{});
     }
 
+    @Put("/save")
     public HttpResponse<Aircraft> save(@Body Aircraft aircraft) {
         return HttpResponse.status(HttpStatus.OK).body(aircraft);
     }
 
+    @Post("/update")
     public HttpResponse<Aircraft> update(@Body Aircraft aircraft) {
         return HttpResponse.status(HttpStatus.OK).body(aircraft);
     }
 
+    @Delete("/delete")
     public HttpResponse<Aircraft> delete(@Body Aircraft aircraft) {
         return HttpResponse.status(HttpStatus.OK).body(aircraft);
     }
