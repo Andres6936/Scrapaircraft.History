441afeeab9538e140db30feabb671a16919edd35
Andres6936 <andres6936@gmail.com>
2024-09-28 20:32:00 -0500

    Add new fields to Aircraft class
    Include new attributes such as type, history, weights, capacity, production, dimensions, powerPlants, performance, countryOrigin, and relatedAircraft for better data representation. This will help in providing more detailed information about each aircraft.

--- src/main/java/com/scap/Aircraft.java
+++ src/main/java/com/scap/Aircraft.java
@@ -5,4 +5,14 @@ import io.micronaut.serde.annotation.Serdeable;
 @Serdeable
 public class Aircraft {
     public String name;
+    public String type;
+    public String history;
+    public String weights;
+    public String capacity;
+    public String production;
+    public String dimensions;
+    public String powerPlants;
+    public String performance;
+    public String countryOrigin;
+    public String relatedAircraft;
 }
