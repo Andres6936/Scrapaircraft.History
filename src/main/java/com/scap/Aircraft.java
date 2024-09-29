734a5d3f10a59048ac7560d9a6053e9106f14771
Andres6936 <andres6936@gmail.com>
2024-09-28 19:51:12 -0500

    Add Aircraft class and basic CRUD methods in controller
    Created the Aircraft class with no properties or methods initially. Added basic CRUD methods (findOne, findAll, save, update, delete) to the AircraftController class returning null for now.

---
+++ src/main/java/com/scap/Aircraft.java
@@ -0,0 +1,5 @@
+package com.scap;
+
+public class Aircraft {
+
+}
--- src/main/java/com/scap/AircraftController.java
+++ src/main/java/com/scap/AircraftController.java
@@ -4,5 +4,23 @@ import io.micronaut.http.annotation.Controller;
 
 @Controller("/aircraft")
 public class AircraftController {
+    public Aircraft findOne() {
+        return null;
+    }
 
+    public Aircraft[] findAll() {
+        return null;
+    }
+
+    public Aircraft save(Aircraft aircraft) {
+        return null;
+    }
+
+    public Aircraft update(Aircraft aircraft) {
+        return null;
+    }
+
+    public Aircraft delete(Aircraft aircraft) {
+        return null;
+    }
 }
